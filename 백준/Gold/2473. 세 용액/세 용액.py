0caefe58a74953f406bb33c86e641b6778087e08
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-02-02 18:48:47 +0900

    [Gold III] Title: 세 용액, Time: 188 ms, Memory: 110604 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,24 +1,20 @@
-n = int(input())
-data = list(map(int, input().split()))
+n=int(input())
+data=list(map(int,input().split()))
 data.sort()
+result=3000000001
+a=()
 
-closest_sum = float('inf')  # 가능한 가장 큰 수로 초기화
-result = []
+for i in range(n-2): ## 2개의 요소는 투 포인터
+    start , end = i+1 , n-1 ## 매 루프를 돌 때 마다 초기 세팅
+    while start < end:
+        value = data[i] + data[start] +data[end]
+        if abs(result) > abs(value):
+            result= value
+            a=(data[i],data[start],data[end])
 
-for i in range(n-2):  # 첫 번째 용액을 고정
-    left, right = i+1, n-1  # 두 번째와 세 번째 용액을 위한 포인터
-    while left < right:
-        current_sum = data[i] + data[left] + data[right]
-        
-        # 더 작은 합을 찾은 경우 업데이트
-        if abs(current_sum) < abs(closest_sum):
-            closest_sum = current_sum
-            result = [data[i], data[left], data[right]]
-        
-        # 합을 조정하기 위해 포인터 이동
-        if current_sum < 0:
-            left += 1
+        if value < 0 :
+            start+=1
         else:
-            right -= 1
+            end-=1
 
-print(' '.join(map(str, result)))
+print(' '.join(map(str,a)))
