7abd64e7d288d43ac74d99908f75e94e1a6cafc1
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-04-04 20:45:46 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 432 ms, Memory: 169680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,20 +1,17 @@
-data=list(input())
-t=int(input())
-for _ in range(t):
-    alpha,l,r=input().split()
-    l=int(l)
-    r=int(r)
-    sum_data=[0 for _ in range(len(data))]
-    if data[0] == alpha:
-        sum_data[0]=1
-
-    for i in range(1,len(data)):
-        sum_data[i]=sum_data[i-1]
-        if data[i]==alpha:
-            sum_data[i]+=1
-
-    if l==0:
-        print(sum_data[r])
+import sys
+input = sys.stdin.readline
+s = input()
+n = int(input())
+re = [[0]*26 for i in range(len(s))]
+for i in range(len(s)):
+    for j in range(26):
+        if ord(s[i])-97==j:
+            re[i][j] = re[i-1][j]+1 #한번 나왔으니 누적합 +1
+        else:
+            re[i][j] = re[i-1][j]#안 나왔으니 누적합
+for _ in range(n):
+    t,x,y = input().split()
+    if int(x)==0:
+        print(re[int(y)][ord(t)-97])#0~y번째까지니 그냥 [y]를 출력해주면 된다.
     else:
-        print(sum_data[r]-sum_data[l-1])
-
+        print(re[int(y)][ord(t)-97]-re[int(x)-1][ord(t)-97])#x~y까지이니 [y]-[x-1]를 출력해주면 된다.
