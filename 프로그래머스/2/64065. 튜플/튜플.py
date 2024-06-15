4e916610cd46dafb6d587757cd41525cc5816702
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-06-15 19:46:35 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,14 +1,18 @@
 def solution(s):
     answer=[]
-    s=s[2:-2]
-    s=s.split("},{")
-    s_sorted=sorted(s,key=lambda x:len(x))
-    for i in s_sorted:
-        ii=i.split(",")
-        for j in ii:
-            if int(j) not in answer:
-                answer.append(int(j))
-   
-
+    s=s[2:-2] # {{ }} 떼버리기
+    s=s.split("},{") # split하면 list가 됨 
+    list_s=[]
+    for part_s in s: # 
+        list_s.append(list(map(int,part_s.split(','))))
+    list_s.sort(key=lambda x: (len(x))) # 각 원소의 길이를 기준으로 정렬 오름차순 
+    
+    
+    for li in list_s: # 작은 길이의 리스트 먼저 순서대로 대입
+        for value in li: # value 값을 차례대로 뽑아냄 
+            if value not in answer:
+                answer.append(value)
+    
+    
 
     return answer
