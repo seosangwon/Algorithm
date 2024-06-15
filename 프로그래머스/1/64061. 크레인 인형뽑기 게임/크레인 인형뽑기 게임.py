11a598f46e95983c698462d306cd1d286271ae66
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-06-15 19:21:17 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.63 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,20 +1,39 @@
 def solution(board, moves):
     answer = 0
-    basket=[]
-    
-    for i in moves:
-        for j in range(len(board)):
-            if board[j][i-1] ==0 :
+    stack=[]
+    N=len(board[0])
+    answer=0
+    for idx in moves:
+        idx-=1 # 1X1 시작이므로 1빼고 시작 
+        value=0 # 뽑기 값
+        for i in range(N):# 행만 움직이면 됨
+            if board[i][idx]==0: # 비어있으면 pass
                 continue
+            else: # 값이 있으면
+                value=board[i][idx]
+                board[i][idx]=0
+                break # 뽑았으면 멈추기 
+        
+        if value==0: # 아예 빈 열이였으면 pass
+            continue
+        
+        if stack: # stack에 값이 있다면
+            prev=stack[-1] # 이전 값은 stack의 최상단 값
+            if prev==value:
+                answer+=2
+                stack.pop()
             else:
-                #새로뽑은 인형과 stack의 top이 같은 인형일 경우
-                if basket and board[j][i-1] == basket[-1]:
-                    basket.pop(-1)
-                    answer+=2
-                    board[j][i-1]=0
-                else: #stack에 쌓아주기
-                    basket.append(board[j][i-1])
-                    board[j][i-1]=0
-                break
-             
+                stack.append(value)
+        else: # stack에 값이 없거나 , 애니팡이 안된다면
+            stack.append(value)
+        
+        
+        
+#         for i in range(N): # 디버깅
+#             for j in range(N):
+#                 print(board[i][j],end=' ')
+#             print()
+#         print()
+            
+
     return answer
