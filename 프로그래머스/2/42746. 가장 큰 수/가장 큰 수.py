0e5d69a0b987edc94a8842dfbb7098656b2e543d
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-06-30 15:32:49 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = ''
+    str_numbers=list(map(str,numbers))
+    str_numbers.sort(key=lambda x:x*3 , reverse=True )
+    answer= str(int(''.join(str_numbers)))
+    
+    
+    
+    return answer
