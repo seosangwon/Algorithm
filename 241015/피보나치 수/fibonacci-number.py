4b8d4eeae9702b700913f2a4bb93f32bd6030390
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-10-15 15:12:59 +0900

    Title: 피보나치 수, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241015/피보나치 수/fibonacci-number.py
@@ -0,0 +1,15 @@
+n=int(input())
+
+dp=[0 for _ in range(n+1)]
+dp[0]=1
+
+if n==2:
+    print(1)
+    
+else:
+    dp[1]=1
+
+    for i in range(3,n+1):
+        dp[i]=dp[i-2]+dp[i-1]
+
+    print(dp[n])
