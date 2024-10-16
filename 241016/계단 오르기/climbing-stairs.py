eeb429cdcd554a77dc17eafb1f24212d3e16226b
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-10-16 12:42:26 +0900

    Title: 계단 오르기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241016/계단 오르기/climbing-stairs.py
@@ -0,0 +1,16 @@
+# 2계단 혹은 3계단으로만 올라갈 수 있다.
+# n층 까지 올라가는 경우의 수를 출력해라
+
+n=int(input())
+dp=[0 for _ in range(n+1)]
+dp[2]=1
+
+if n==2:
+    print(1)
+
+else:
+    for i in range(3,n+1):
+        dp[i]+=dp[i-2]+1
+        dp[i]+=dp[i-3]+1
+
+    print(dp[n])
