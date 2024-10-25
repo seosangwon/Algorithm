33fcdf175a1136cb0dfd4dbfb718b8dcb44ef5cc
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-10-25 20:16:44 +0900

    Title: 최대 감소 부분 수열, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241025/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -0,0 +1,16 @@
+n=int(input())
+data=list(map(int,input().split()))
+MIN_INF=-int(1e9)
+dp=[MIN_INF]*n
+dp[0]=1
+
+for i in range(1,n):
+    for j in range(i):
+        if dp[j]==MIN_INF:
+            dp[j]=1
+        
+        if data[j]>data[i]:
+            dp[i]=max(dp[i],dp[j]+1)
+
+
+print(max(dp))
