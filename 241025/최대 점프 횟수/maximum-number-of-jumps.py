ea4810b06f82780d6fad55204d965cf060e3c2c8
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-10-25 20:29:42 +0900

    Title: 최대 점프 횟수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241025/최대 점프 횟수/maximum-number-of-jumps.py
@@ -0,0 +1,17 @@
+n=int(input())
+data=list(map(int,input().split()))
+
+MIN_INF=-int(1e9)
+dp=[MIN_INF] * n 
+dp[0]=0
+
+for i in range(1,n):
+    for j in range(i):
+        if dp[j]==MIN_INF:
+            continue
+        
+        if i <= j+data[j]:
+            dp[i]=max(dp[i],dp[j]+1)
+
+print(dp)
+print(max(dp))
