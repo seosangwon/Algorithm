f95305788443832cc9e1ab6922f3a90cced7b262
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-10-25 20:04:56 +0900

    Title: 최대 증가 부분 수열, Time: 162ms, Memory: 25MB, Status: Passed - CodeTree

--- 241025/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 241025/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -8,7 +8,7 @@ dp[0]=1
 for i in range(1,n):#dp[i]는 점프 될 곳 
     for j in range(0,i): # dp[j]에서 점프해서 dp[i]로 가자
         if dp[j]==MIN_INF:
-            continue
+            dp[j]=1
         
         if data[j]<data[i]:
             dp[i]=max(dp[i],dp[j]+1) # dp[j]를 거치는 경우, 안거치는 경우 
