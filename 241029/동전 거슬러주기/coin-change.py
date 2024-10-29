1d47f82b2aa769162e817b7e5e9614b3b8554fbd
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-10-29 20:16:17 +0900

    Title: 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241029/동전 거슬러주기/coin-change.py
@@ -0,0 +1,20 @@
+#금액 M을 맞추는 최소 동전의 개수를 출력해라
+#i는 금액 
+#dp[i]는 금액i를 거슬러 줄 수 있는 최소 동전의 수 
+n,m=map(int,input().split())
+coins=list(map(int,input().split()))
+coins.sort()
+INF=int(1e9)
+dp=[INF]*(m+1)
+
+for coin in coins:
+    dp[coin]=1
+
+
+for i in range(1,m+1): # 현재 금액 
+    for j in range(n):   
+        if i>coins[j]:
+            dp[i]=min(dp[i],dp[i-coins[j]]+1)
+
+
+print(dp[-1])
