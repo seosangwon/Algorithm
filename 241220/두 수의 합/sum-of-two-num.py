a461d0e8f12552331c819f581c4c39aea29f02e3
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-12-20 21:53:08 +0900

    Title: 두 수의 합, Time: 1010ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241220/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,29 @@
+#시간복잡도 : O(nlogn)
+n,k=map(int,input().split())
+nums=list(map(int,input().split()))
+hash_map={}
+answer=0
+min_num=min(nums)
+max_num=max(nums)
+
+#전부 0으로 초기화
+for n in range(-10**9 , 10**9+1):
+    hash_map[n]=0
+
+#딕셔너리에 갯수 추가 
+for n in nums:
+    hash_map[n]+=1
+
+#중복 값 제거 
+nums_set=set(nums)
+
+for n in nums_set:
+    r=k-n
+    answer+=hash_map[n]*hash_map[r]
+        
+    
+print(answer)
+        
+    
+        
+
