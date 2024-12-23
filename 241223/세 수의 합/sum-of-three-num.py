284815d59861ffc131709d63005dd81f8df93743
seosangwon <39723498+seosangwon@users.noreply.github.com>
2024-12-23 18:07:18 +0900

    Title: 세 수의 합, Time: 57ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/세 수의 합/sum-of-three-num.py
+++ 241223/세 수의 합/sum-of-three-num.py
@@ -3,33 +3,36 @@ from collections import defaultdict
 n,k=map(int,input().split())
 nums=list(map(int,input().split()))
 hash_map=defaultdict(int)
+len_=n
 
 for n in nums:
     hash_map[n]+=1
 
 result=0
+idx=0
 for e1 in nums:
-    for e2 in nums:
+    idx+=1
+    for e2 in nums[idx:]:
         e3=k-(e1+e2)
         v1,v2,v3 = hash_map[e1],hash_map[e2],hash_map[e3]
         
         if e1==e2 and e1==e3:
             result+=(v1 *(v2-1) * (v3-2))
-            print(1,e1,e2,e3)
+            #print(1,e1,e2,e3)
         elif e1==e2 and e2!=e3:
             result+=(v1*(v2-1)*v3)
-            print(2,e1,e2,e3)
+            #print(2,e1,e2,e3)
         elif e1==e3 and e2!=e3:
             result+=(v1*v2*(v3-1))
-            print(3,e1,e2,e3)
+            #print(3,e1,e2,e3)
         elif e2==e3 and e1!=e2:
             result+=(v1*v2*(v3-1))
-            print(4,e1,e2,e3)
+            #print(4,e1,e2,e3)
         elif e1!=e2 and e2!=e3:
             result+=(v1*v2*v3)
-            print(5,e1,e2,e3)
+            #print(5,e1,e2,e3)
 
 
-print(result//12)
+print(result//(12*len_))
 
  
