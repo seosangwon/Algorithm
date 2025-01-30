264c6ac2ceed5d9b4661b9ae204e2458b33f38fa
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-01-30 17:04:59 +0900

    Title: 정원 입장은 선착순, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250130/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -0,0 +1,65 @@
+# 1. 현재 종료시간을 알아야 함 
+# 2. 대기 큐에 입장 가능한 사람들을 우선순위로 정렬 
+# 3. 입장 가능하면은 입장 
+# 4. 종료시간 갱신 
+# 5. 최고 오래 기다린 시간 갱신 
+# 6. 대기 큐에 더 이상 사람이 없으면 실행 종료 
+
+import heapq
+from collections import deque
+N=int(input())
+a=[]
+t=[]
+for i in range(N):
+    s,e=map(int,input().split())
+    a.append([s,i]) # [입장 시각 , idx]
+    t.append(e) # 소요시간 저장 
+
+a=deque(sorted(a))
+s_q=[] # 대기 큐 
+f_start,f_idx=a.popleft()
+heapq.heappush(s_q,[f_idx , f_start]) # 첫번째 사람 대기열에 넣기 
+
+
+cur_time=f_start
+answer=0
+
+
+
+while s_q: # 대기 큐에 더이상 대기자가 없으면 종료 
+    cur_idx , cur_start = heapq.heappop(s_q)
+    answer=max(answer , (cur_time-cur_start))
+    cur_time+=t[cur_idx] # 소요 시간 더해주기 
+
+    del_count=0
+    if a: # 아직 입장 안한 사람들이 있다면은 
+        for a_time , idx in a:  #[입장시각 , idx]
+            if a_time <= cur_time: # 입장 가능하면은 
+                heapq.heappush(s_q,[idx,a_time])
+                del_count+=1
+            else:
+                break
+    
+    for _ in range(del_count):
+        a.popleft()
+    
+    
+
+print(answer)
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
