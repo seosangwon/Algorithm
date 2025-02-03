ff636118394f788a45e096d76a3aa0e8beb947d5
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-02-03 11:22:01 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 1024ms, Memory: 21MB, Status: Wrong Answer - CodeTree

---
+++ 250203/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.py
@@ -0,0 +1,32 @@
+from collections import deque
+n = int(input())
+arr = list(map(int, input().split()))
+
+
+# 시간복잡도 n은 최대 1e5
+# 구각 내의 중복되는 숫자가 없는지 어떻게 확인할것인가??
+# in 연산자 써서 arr[right]의 값이 couting_array에 있으면은 arr[left]값을 제거 하고 left+=1 없으면은 갱신 
+answer=1
+left,right=0,0
+count_arr=deque([arr[0]])
+while left<=right and right<n-1:
+    right+=1
+    
+    if arr[right] in count_arr: # 있으면은 left 값 제거 
+        count_arr.popleft()
+        left+=1
+    
+    else:
+        answer=max(answer , right-left+1)
+    
+    count_arr.append(arr[right])
+    
+    
+
+
+print(answer)
+        
+
+
+
+
