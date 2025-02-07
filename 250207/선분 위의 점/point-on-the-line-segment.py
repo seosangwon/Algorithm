c9b8047165100c107d782ca4a724138f959b3c08
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-02-07 15:20:59 +0900

    Title: 선분 위의 점, Time: 143ms, Memory: 23MB, Status: Wrong Answer - CodeTree

--- 250207/선분 위의 점/point-on-the-line-segment.py
+++ 250207/선분 위의 점/point-on-the-line-segment.py
@@ -5,17 +5,15 @@ segments = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Write your code here!
 # 선분위에 점이 몇개 있는지 구한다 
-# 선분에 점을 각각 순회 했을 때 , 그 사이 값이면은 점이 있는 것이다 
 
-for x,y in segments: # 선분 순회 
-    answer=0
-    line=[x,y]
-    for p in points: # 점 순회 
-        idx=bisect.bisect_right(line,p)
-        if idx==1 or p==line[idx-1]:
-            answer+=1
+for x,y in segments: # 선분의 시작과 끝 순차 조회 
+    x_idx=bisect.bisect_left(points,x)
+    y_idx=bisect.bisect_right(points,y)
     
+    answer = y_idx - x_idx
     print(answer)
-        
+
+
+
 
 
