5127e1f05f3495ff4b3e01265010a91a6b5df036
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-02-13 11:10:34 +0900

    Title: 삼 오 무, Time: 53ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250213/삼 오 무/three-five-moo.py
+++ 250213/삼 오 무/three-five-moo.py
@@ -2,18 +2,27 @@ N = int(input())
 
 # N번째로 적히는 숫자를 출력한다 
 # 순회하는 방법은 O(N)
-num=1
-for i in range(N-1):
-    num+=1
-    while num%3 == 0 or num % 5==0:
-        num+=1
-    
-        
+left=1
+right=int(1e9)
 
-print(num)
-    
-    
+answer=right
 
+def is_possible(k):
+    cnt= k - (k//3+k//5+k//15)
+
+    return cnt
+
+
+while left<=right:
+    mid=(left+right)//2
+
+    if is_possible(mid) >= N :  # 서로 다른 숫자의 개수가 N개 이상이라면은 최소 값을 출력한다 
+        right=mid-1
+        answer=min(answer,mid)
+    else:
+        left=mid+1
+
+print(answer)
 
 
 
