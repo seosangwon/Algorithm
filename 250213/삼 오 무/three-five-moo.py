d496f157aeedb45f8eeb9b92293f1b359f7f65fc
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-02-13 10:56:38 +0900

    Title: 삼 오 무, Time: 66ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250213/삼 오 무/three-five-moo.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+# N번째로 적히는 숫자를 출력한다 
+# 순회하는 방법은 O(N)
+num=1
+for i in range(N):
+    num+=1
+    while num%3 == 0 or num % 5==0:
+        num+=1
+    
+        
+
+print(num-1)
+    
+    
+
+
+
+
