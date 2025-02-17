40e0e09389ab2f99df160ca607d3208c3aab0076
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-02-17 10:13:54 +0900

    Title: 집합의 원소, Time: 621ms, Memory: 26MB, Status: Passed - CodeTree

--- 250217/집합의 원소/elements-of-a-set.py
+++ 250217/집합의 원소/elements-of-a-set.py
@@ -1,43 +1,33 @@
-# 변수 선언 및 입력:
-n, m = tuple(map(int, input().split()))
-uf = [0] * (n + 1)
-
-# 초기 uf 값을 설정합니다.
-for i in range(1, n + 1):
-    uf[i] = i
+n, m = map(int, input().split())
 
 
-# x의 대표 번호를 찾아줍니다.
-def find(x):
-    # x가 루트 노드라면 x값을 반환합니다.
-    if uf[x] == x:
-        return x
-    # x가 루트 노드가 아니라면
-    # x의 부모인 uf[x]에서 탐색을 더 진행한 뒤
-    # 찾아낸 루트 노드를 uf[x]에 넣어줌과 동시에
-    # 해당 노드값을 반환합니다.
-    uf[x] = find(uf[x])
-    return uf[x]
+# 0 이면 두 노드 합치기 
+# 1 이면 find 해서 같은 집합이면 1 출력 , 아니면 0 출력 
+
+parent_node=[i for i in range(n+1)]
 
 
-# x, y가 같은 집합이 되도록 합니다.
-def union(x, y):
-    # x, y의 대표 번호를 찾은 뒤
-    # 연결해줍니다.
-    X, Y = find(x), find(y)
-    uf[X] = Y
+def find(n):
+    if parent_node[n]==n:
+        return n 
+    root_node=find(parent_node[n])
+    parent_node[n]=root_node
+    return root_node
+
+def union(n1,n2):
+    r_n1= find(n1)
+    r_n2= find(n2)
+    
+    parent_node[r_n2]=r_n1
+        
 
 
 for _ in range(m):
-    q_type, a, b = tuple(map(int, input().split()))
-
-    # 합치는 명령입니다.
-    if q_type == 0:
-        union(a, b)
-    # 같은 집합에 있는지를 판단하는 명령입니다.
-    else:
-        # 같은 집합인 경우에는 1을 출력합니다.
-        if find(a) == find(b):
+    command,n1,n2=map(int,input().split())
+    if command==0:
+        union(n1,n2)
+    if command==1:
+        if find(n1) == find(n2):
             print(1)
         else:
             print(0)
