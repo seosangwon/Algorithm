9fb039e43f359967f8009fa5dad62b13663d3c07
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-02-25 10:37:21 +0900

    Title: 최대 동전 거슬러주기, Time: 52ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250225/최대 동전 거슬러주기/max-coin-change.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+coin = list(map(int, input().split()))
+
+
+# 최대 동저의 개수로 거스름돈 거슬러주기 
+
+dp=[0]*(M+1)
+
+for c in coin: # 각 동전 초기화 
+    dp[c]=1
+
+coin_max=max(coin)
+
+for i in range(coin_max+1,M+1):
+    for j in range(N):
+        
+        dp[i]=max(dp[i],(dp[i-coin[j]]+1))
+
+print(dp[M])
