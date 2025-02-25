dcd19fdec2f95073f4d395f243e6a35b78964faf
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-02-25 10:21:16 +0900

    Title: 1, 2, 5 더하기, Time: 53ms, Memory: 16MB, Status: Passed - CodeTree

--- 250225/1, 2, 5 더하기/1-2-5-plus.py
+++ 250225/1, 2, 5 더하기/1-2-5-plus.py
@@ -1,15 +1,15 @@
 # 1,2,5의 합으로 나타내는 방법의 수 
 N=int(input())
 dp=[0]*(10001)
-dp[1]=1
-dp[2]=2
-dp[3]=3
-dp[4]=5
-dp[5]=9
+dp[0]=1
+numbers=[1,2,5]
 
-if N>5:
-    for i in range(6,N+1):
-        dp[i]=max(dp[i],(dp[i-1]+dp[i-2]+dp[i-5])%10007)
+
+for i in range(1,N+1):
+    for j in range(3):
+        if i>=numbers[j]:
+            dp[i]=(dp[i]+dp[i-numbers[j]])%10007
 
 print(dp[N])
+
         
