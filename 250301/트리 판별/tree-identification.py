b3815c735546a306ecd18f72f14feb25c9bd9a4d
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-03-01 21:16:38 +0900

    Title: 트리 판별, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250301/트리 판별/tree-identification.py
+++ 250301/트리 판별/tree-identification.py
@@ -1,101 +1,65 @@
 m = int(input())
-edges=[]
-# 트리 판별 요건 : 사이클이 없는지 확인 , 루트 노드에서 모든 노드로 이동 할 수 있는지 확인 
-
-# 1. 루트 노드 찾기 
-# 2. dfs로 전체 순회 
-# 3. 사이클이 발생하면은 return 0 
-# 4. dfs 종료 후 , 모든 노드가 방문 처리 되어 있다면은 return 1 
-
-# 루트 노드 찾기 
-max_node=-1
+edges = []
+nodes = set()  # 실제 노드 집합
 
 for _ in range(m):
-    s,e=map(int,input().split())
-    edges.append([s,e])
-    max_node=max(max_node , s, e )
+    s, e = map(int, input().split())
+    edges.append([s, e])
+    nodes.add(s)
+    nodes.add(e)
 
-in_edges=[[] for _ in range(max_node+1)]
-out_edges=[[] for _ in range(max_node+1)]
-for s,e in edges:
+# 최대 노드 번호 대신 실제 노드 개수에 기반한 인덱스 재구성 (노드 번호가 연속적이지 않을 수 있음)
+max_node = max(nodes)
+
+# in_edges, out_edges 초기화: 0부터 max_node까지 생성 (노드가 존재하지 않더라도 공간 확보)
+in_edges = [[] for _ in range(max_node + 1)]
+out_edges = [[] for _ in range(max_node + 1)]
+for s, e in edges:
     in_edges[e].append(s)
     out_edges[s].append(e)
 
-def find_root(in_edges):
-    root_node=-1
-    cnt=0
-    for i in range(1,max_node+1):
-        if len(in_edges[i])==0:
-            root_node=i 
-            cnt+=1
-        if cnt>=2:
+def find_root(nodes, in_edges):
+    root_node = -1
+    cnt = 0
+    # nodes 집합에 있는 노드만 확인
+    for i in nodes:
+        if len(in_edges[i]) == 0:
+            root_node = i 
+            cnt += 1
+        if cnt >= 2:
             break 
-    
-    if cnt>=2:
+    if cnt != 1:
         return -1 
-    
     return root_node
 
-
-visited=[False]*(max_node+1)
-flag=True
+# 방문 체크를 위해 실제 노드에 대해서만 관리
+visited = {node: False for node in nodes}
+flag = True
 
 def dfs(node):
     global flag
-    visited[node]=True  # 노드 방문 처리 
-
+    visited[node] = True  # 노드 방문 처리 
     for next_node in out_edges[node]:
-        if visited[next_node]: # 만약 방문이 이미 되어있다면 경로가 유일하지 않은 것 
-            flag=False 
+        if next_node in visited:  # next_node가 실제 노드인지 확인
+            if visited[next_node]:  # 이미 방문했다면 사이클
+                flag = False 
+            else:
+                dfs(next_node)
 
-        
-        if not visited[next_node]:
-            dfs(next_node)
-    
-    
-        
-        
-
-            
-
-
-
-def is_root():
-    root_node = find_root(in_edges)
-    if root_node== -1: # 루트 노드가 여러개 
-        # print("루트 노드 여러개 ")
-        # print(in_edges)
+def is_tree():
+    root_node = find_root(nodes, in_edges)
+    if root_node == -1:  # 루트 노드가 여러 개거나 없음
         return 0
     
     dfs(root_node)
-    if not flag: # 사이클 존재 
-        # print("사이클 존재")
+    
+    if not flag:  # 사이클 발생
         return 0 
 
-    
-    if False in visited[1:] : # 모든 노드 존재 방문 
-        # print("모든 노드 방문 x ")
+    # 실제 노드 집합의 모든 노드가 방문되었는지 확인
+    if not all(visited[node] for node in nodes):
         return 0 
-    
     
     return 1 
 
-
-print(is_root())
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-
+print(is_tree())
