8789eb0b2f8441239788ae8ac7302f2891a59c02
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-03-08 21:33:41 +0900

    [level 2] Title: 비밀 코드 해독, Time: 355.79 ms, Memory: 22.4 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -3,24 +3,23 @@ from itertools import combinations
 # 시스템 응답 0~10 
 def solution(n, q, ans):
     answer=0
-    lst=[i for i in range(1,n+1)]
-    index=list(filter(lambda i : ans[i] == 0 , range(len(ans))))
-    m=len(ans)
-    
-    for i in index:
-        for j in q[i]:
-            try: lst.remove(j)
-            except: pass
-    for c in combinations(lst,5):
-        for i in range(m):
+    cmb_list=list(combinations(range(1,n+1),5))
+    n=len(q)
+    m=len(q[0])
+    for li in cmb_list:
+        flag=True
+        for i in range(n): # 행 idx
             cnt=0
-            for j in q[i]:
-                if j in c:
-                    cnt +=1
+            if flag:
+                for j in range(m): # 열 idx
+                    if li[j] in q[i]:
+                        cnt+=1
             if cnt != ans[i]:
-                break
-        else:
+                flag=False 
+        
+        if flag:
             answer+=1
+            
                 
     
     return answer
