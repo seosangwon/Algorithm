0018dc6d653fc920ede6711afa64afb7b96bb397
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-03-13 12:46:42 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 1395ms, Memory: 28MB, Status: Passed - CodeTree

--- 250313/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
+++ 250313/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
@@ -7,47 +7,34 @@ segments = [tuple(map(int, input().split())) for _ in range(M)]
 left=0
 right=int(sys.maxsize)
 
-max_line = -int(1e9)
-min_line=int(1e9)
-
-for a,b in segments:
-    max_line=max(max_line,b)
-    min_line=min(min_line,a)
-
-is_line=[False]*(max_line +1 )
-
-for a,b in segments:
-    for i in range(a,b+1):
-        is_line[i]=True 
-
-
-
-
+MAX_NUM=1e18
 
 def is_possible(dist):
-    cnt=1
-    idx=min_line
-    while idx <=(max_line-dist) :
-        if is_line[idx+dist]: # 점을 놓을 수 있다면은 
-            cnt+=1 
-            idx+=dist 
-        else:
-            idx+=1 
-        
-    return cnt >= N 
+    cnt=0
+    last_x= - MAX_NUM
+    for a,b in segments:
+        while last_x + dist <=b :
+            cnt+=1
+            last_x=max(a,last_x+dist)
 
+            if cnt>=N:
+                break
+    
+    return cnt>=N 
 
+segments.sort()
 
-
+answer=0
 while left<=right:
     mid=(left+right)//2 
     if is_possible(mid): # 만족하면은 거리 값을 늘려도 됨 
         left=mid+1 
+        answer=max(answer,mid)
     else: # 불만족하면은 거리 값을 줄여야 함 
         right=mid-1 
 
 
-print(left-1)
+print(answer)
         
 
 
