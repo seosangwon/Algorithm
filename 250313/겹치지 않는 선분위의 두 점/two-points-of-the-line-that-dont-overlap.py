da429b8f5775ce4a7d6f008696c4d5b9f778c355
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-03-13 12:36:04 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 89ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 250313/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.py
@@ -0,0 +1,48 @@
+import sys 
+# N개의 점 , M개의 선분 
+N, M = map(int, input().split())
+segments = [tuple(map(int, input().split())) for _ in range(M)]
+
+# 가장 가까운 두 점의 거리의 최댓값을 출력해라 
+left=0
+right=int(sys.maxsize)
+
+max_line = max(segments[-1])
+min_line=min(segments[0])
+is_line=[False]*(max_line +1 )
+
+for a,b in segments:
+    for i in range(a,b+1):
+        is_line[i]=True 
+
+
+
+
+
+def is_possible(dist):
+    cnt=1
+    idx=min_line
+    while idx <=(max_line-dist) :
+        if is_line[idx+dist]: # 점을 놓을 수 있다면은 
+            cnt+=1 
+            idx+=dist 
+        else:
+            idx+=1 
+        
+    return cnt >= N 
+
+
+
+
+while left<=right:
+    mid=(left+right)//2 
+    if is_possible(mid): # 만족하면은 거리 값을 늘려도 됨 
+        left=mid+1 
+    else: # 불만족하면은 거리 값을 줄여야 함 
+        right=mid-1 
+
+
+print(left-1)
+        
+
+
