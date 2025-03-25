748849ac2586b1f50935ef77997f68649868cb4a
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-03-25 11:51:25 +0900

    [level 2] Title: 완전범죄, Time: 1.29 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,31 +1,41 @@
-# 각 물건에 대해 A도둑과 B 도둑이 남기는 흔적의 개수는 1 이상 3 이하이다 
-# A 도둑이 남긴 흔적의 누적 개수가 최솟값을 return
-# 만약 두 도둑 모두 경찰에 붙잡히지 않게 할 수 없다면 -1을 return 
+# A가 물건을 훔쳤을 경우
+# B가 물건을 훔쳤을 경우 
+
 def solution(info, n, m):
-    length=len(info)
+    answer = 0
     INF=int(1e9)
-    dp=[[INF]*(m) for _ in range(length+1)]
+    length=len(info)
+    dp=[[INF]*m for _ in range(length+1)]
     dp[0][0]=0
     
     for i in range(length):
-        a_cost=info[i][0]   
-        b_cost=info[i][1]
+        A_cost=info[i][0]
+        B_cost=info[i][1]
         for j in range(m):
             if dp[i][j]==INF:
                 continue 
             
             # A가 훔치는 경우 
-            new_a=dp[i][j]+a_cost 
+            new_cost_A=A_cost+dp[i][j]
+            if dp[i+1][j] > new_cost_A:
+                dp[i+1][j]=new_cost_A
             
-            if new_a < dp[i+1][j]:
-                dp[i+1][j]=new_a
+            # B가 훔치는 경우 
+            new_cost_B=B_cost + j
             
-            #B가 훔치는 경우 
-            new_b=j+b_cost 
-            if new_b<m:
-                if dp[i][j] < dp[i+1][new_b]:
-                    dp[i+1][new_b]=dp[i][j]
+            if new_cost_B < m:
+                if dp[i+1][new_cost_B] > dp[i][j]:
+                    dp[i+1][new_cost_B]=dp[i][j]
+                    
+    # for i in range(length+1):
+    #     for j in range(m):
+    #         print(dp[i][j],end=' ')
+    #     print()
     
     answer=min(dp[length])
     
-    return answer if answer<n else -1
+    if answer>=n:
+        return -1
+
+    
+    return answer
