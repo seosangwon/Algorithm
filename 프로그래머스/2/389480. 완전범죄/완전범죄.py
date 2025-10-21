9c3ffa1110ff5b0daf1db00fe8ae038d92e1a9b9
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-10-21 21:37:10 +0900

    [level 2] Title: 완전범죄, Time: 2.45 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,41 +1,42 @@
-# A가 물건을 훔쳤을 경우
-# B가 물건을 훔쳤을 경우 
-
+# A도둑의 흔적을 가장 적게 만들어 return
+# A도둑이 훔치느냐 B도둑이 훔치느냐 : 최대 물건의 개수는 40개이므로 완탐시 2**40으로 시간초과
+# memoization이 필요함 -> dp
+# A와 훔쳤을 경우와 B 훔쳤을 경우를 나눈다
+# 행은 idx, 열은 m
 def solution(info, n, m):
-    answer = 0
-    INF=int(1e9)
-    length=len(info)
-    dp=[[INF]*m for _ in range(length+1)]
-    dp[0][0]=0
-    
-    for i in range(length):
-        A_cost=info[i][0]
-        B_cost=info[i][1]
-        for j in range(m):
-            if dp[i][j]==INF:
-                continue 
-            
-            # A가 훔치는 경우 
-            new_cost_A=A_cost+dp[i][j]
-            if dp[i+1][j] > new_cost_A:
-                dp[i+1][j]=new_cost_A
-            
-            # B가 훔치는 경우 
-            new_cost_B=B_cost + j
-            
-            if new_cost_B < m:
-                if dp[i+1][new_cost_B] > dp[i][j]:
-                    dp[i+1][new_cost_B]=dp[i][j]
-                    
-    # for i in range(length+1):
-    #     for j in range(m):
-    #         print(dp[i][j],end=' ')
-    #     print()
-    
-    answer=min(dp[length])
-    
-    if answer>=n:
-        return -1
 
+    INF=int(1e9)
+    len_=len(info)
+    dp=[[INF]*m for _ in range(len_+1)]
+    dp[0][0]=0
+    for i in range(len_):
+        a=info[i][0]
+        b=info[i][1]
+        for j in range(m):
+            if dp[i][j]==INF: # 도달하지 않은 값이면 경우의 수가 없는 것, 패스한다
+                continue
+            
+            #A가 가져갔을 경우
+            if dp[i][j]+a<n: # n보다 작아야함 
+                dp[i+1][j]=min(dp[i][j]+a,dp[i+1][j])
+            
+            #B가 가져갔을 경우
+            if j+b<m:
+                dp[i+1][j+b]=min(dp[i][j],dp[i+1][j+b])
+    
+    
+    for i in range(len_+1):
+        for j in range(m):
+            print(dp[i][j],end=' ')
+        print()
+    
+    
+    answer = min(dp[len_]) 
+    
+    
+    if answer==INF:
+        answer=-1
+    
+                
     
     return answer
