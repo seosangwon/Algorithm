ac1db6059d01daceb924432e77a12d4b9c75cea2
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-12-07 13:16:25 +0900

    Title: 적석과 흑석 2, Time: 451ms, Memory: 25MB, Status: Wrong Answer - Codetree

--- 251207/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 251207/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -2,7 +2,7 @@
 # 시간제한 4초 
 # C,N이 최대 1e5이므로 최소 ClongN
 # 1. 2중 탐색 - O(N^2) X , 2.    
-# 빨간공 제일 값이 큰 애 부터 검은공  A,B에 끼워 넣는다. 
+# 빨간공 제일 값이 작은거 부터 처리한다.
 # 1. 검은공 순차탐색 2. 조건에 충족한 빨간공이 있으면 +1 3. 조건에 충족한 빨간공이 없으면 빨간공 큐에서 제거 
 
 import heapq
@@ -13,43 +13,36 @@ black_q=[]
 
 for _ in range(C):
     red=int(input())
-    red_q.append(red*-1)
+    red_q.append(red)
 
 for _ in range(N):
     a,b=map(int,input().split(" "))
     black_q.append((a,b))
 
-black_q.sort(key=lambda x: (-x[0],-x[1]))
-
-answer=0
+black_q.sort(key=lambda x: (x[0],x[1]))
 heapq.heapify(red_q)
+answer=0
 
-# business logic 
 for a,b in black_q:
-    
-    # red_q를 순차탐색 
+
     while red_q:
-        red=red_q[0]*-1
+        
+        if red_q[0] > b: # red중에 제일 작은 값인데 b 보다 커버리면 이 후 원소들 모두 조건 불충족 
+            break
 
-        if red<a : # red의 max 값이 a보다 작다면  그 밑의 원소들은 모두 충족하지 않음 
-            break 
-
-        # 조건을 충족한다면 
-        if a<=red<=b:
+        if red_q[0] < a :  # 조건 미충족시 원소 제거 후 다시 로직을 탄다.
+            heapq.heappop(red_q)
+            continue 
+        
+        if a<=red_q[0]<=b: # 조건 충족시 원소 제거 후 break
             answer+=1
             heapq.heappop(red_q)
-            break 
-
-        if a<=red and red>b:
-            heapq.heappop(red_q)
-
+            break
         
-                
-
 
 print(answer)
-
-
+        
+        
 
 
 
