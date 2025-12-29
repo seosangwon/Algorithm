6b487b3ed709c16601bac69fc5303f3ba1368897
seosangwon <39723498+seosangwon@users.noreply.github.com>
2025-12-29 20:42:24 +0900

    Title: G & H 반전시키기, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251229/G & H 반전시키기/reversing-g-and-h.py
@@ -0,0 +1,29 @@
+# 같은 구간을 2번 뒤집는거는 사실상 같은행위다 ok 
+# 바꿔야 할 부분만 찾아서 뒤집는다는 사실상 위와 같은 풀이인것이다.
+# a와 b를 비교하다가 다른 부분이 있으면 체크한다.
+
+N = int(input())
+a = input()
+b = input()
+answer=0
+flag=False # 연속을 체크하는 플래그 변수 
+
+for i in range(N):
+    if a[i]!=b[i] and !flag:
+        answer+=1
+    
+    if a[i]!=b[i] and flag:
+        continue 
+    
+    
+    if a[i]==b[i]:
+        flag=False    
+    
+
+    
+    
+        
+
+print(answer)
+
+
