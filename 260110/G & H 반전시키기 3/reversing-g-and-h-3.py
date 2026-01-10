2938c5bab5171927d60a499144a12246278f5977
seosangwon <39723498+seosangwon@users.noreply.github.com>
2026-01-10 14:40:40 +0900

    Title: G & H 반전시키기 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260110/G & H 반전시키기 3/reversing-g-and-h-3.py
@@ -0,0 +1,36 @@
+# 한번에 뒤집을 수 있는 최대 크기는 4
+# 앞에게 반대여서 뒤집어야 한다 vs 내가 반대여서 뒤집어야 한다. 
+# 결국 2번 뒤집는건 의미가 없음
+# 내가 반대면은 뒤집는다 최대 4칸까지 봐주면서 뒤집는다. 
+# 맞으면 패스 
+
+
+n=int(input())
+a=list(input())
+b=list(input())
+
+
+#변수
+answer=0
+
+
+#함수
+def flip(x,n):
+    for i in range(4):
+        if x+i<n:
+            if a[x+i]!=b[x+i]:
+                if a[x+i]=='G':
+                    a[x+i]='H'
+                elif a[x+i]=='H':
+                    a[x+i]='G'
+        
+
+
+# 그리디
+for i in range(n):
+    if a[i]!=b[i]:
+        flip(i,n) # 최대 4칸까지 뒤집어주기 
+        answer+=1
+
+print(answer)
+
