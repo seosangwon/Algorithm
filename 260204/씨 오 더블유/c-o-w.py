a9e857cad5af2585731502e7f38c07ef5f3c955f
seosangwon <39723498+seosangwon@users.noreply.github.com>
2026-02-04 19:58:41 +0900

    Title: 씨 오 더블유, Time: 59ms, Memory: 22MB, Status: Passed - Codetree

--- 260204/씨 오 더블유/c-o-w.py
+++ 260204/씨 오 더블유/c-o-w.py
@@ -1,19 +1,18 @@
-import sys
-input = sys.stdin.readline
+n=int(input())
+s=list(input())
 
-N = int(input().strip())
-s = input().strip()
-
-c_count = 0
-co_count = 0
-cow_count = 0
+c_count=0
+co_count=0
+cow_count=0
 
 for ch in s:
-    if ch == 'C':
-        c_count += 1
-    elif ch == 'O':
-        co_count += c_count
-    elif ch == 'W':
-        cow_count += co_count
+    if ch=='C':
+        c_count+=1
+    elif ch=='O':
+        co_count+=c_count
+    elif ch=='W':
+        cow_count+=co_count
+
 
 print(cow_count)
+    
